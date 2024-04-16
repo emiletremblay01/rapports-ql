edd7bc44b12ba7439263fa3dfd1b64f8273ba93e
Emile Tremblay <emiletremblay01@gmail.com>
2024-04-16 02:30:38 -0400

    added pinned card ui

--- components/sidebar-right.tsx
+++ components/sidebar-right.tsx
@@ -30,10 +30,24 @@ export default function SidebarRight({
   return (
     <div id="sidebar-right" className={cn(className, "border-l h-screen")}>
       <div className="flex flex-col gap-2 items-baseline">
-        <h1>PINNED</h1>
+        <h1 className="w-full text-center text-xl font-bold py-4">
+          PINNED POSTS
+        </h1>
         <Card className="rounded-xl">
-          <CardContent className="">
-            <p>Card Content</p>
+          <CardContent className="text-sm py-4">
+            <p className="text-sm pb-2">
+              Coming shortly, 𝕏 will publish everything demanded by @Alexandre
+              and how those requests violate Brazilian law.
+              <br /> This judge has brazenly and repeatedly betrayed the
+              constitution and people of Brazil. He should resign or be
+              impeached. Shame @Alexandre, shame.
+            </p>
+            <div className="w-full flex">
+              <div className="inline-flex items-baseline gap-1">
+                <h1 className="text-muted-foreground">Shayla Ngawala</h1>
+                <p className="text-muted-foreground">· 22h</p>
+              </div>
+            </div>
           </CardContent>
         </Card>
       </div>
