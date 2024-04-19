3e13ba43368c84e3ec6dc6b7a2375fd8a18567b9
Emile Tremblay <76021392+emiletremblay01@users.noreply.github.com>
2024-04-18 20:24:11 -0400

    added router hook

--- app/(root)/post/page.tsx
+++ app/(root)/post/page.tsx
@@ -1,1 +1,10 @@
-qa
+import { useRouter } from "next/navigation";
+
+export default function PostPage() {
+  const router = useRouter();
+  return (
+    <main className="flex min-h-screen flex-col items-center w-full max-w-2xl">
+      post something
+    </main>
+  );
+}
