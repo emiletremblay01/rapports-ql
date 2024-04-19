2e35028cfb85bd4fcc966eebec801e43de4883c9
Emile Tremblay <76021392+emiletremblay01@users.noreply.github.com>
2024-04-18 21:08:22 -0400

    changed router to link

--- app/(root)/post/page.tsx
+++ app/(root)/post/page.tsx
@@ -1,10 +1,5 @@
-import { useRouter } from "next/navigation";
-
+import Link from 'next/link'
+ 
 export default function PostPage() {
-  const router = useRouter();
-  return (
-    <main className="flex min-h-screen flex-col items-center w-full max-w-2xl">
-      post something
-    </main>
-  );
+  return <Link href="/">back home</Link>
 }
