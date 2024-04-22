a70452ad94ccd950c26a02e507efe1f01e42faa0
Emile Tremblay <emiletremblay01@gmail.com>
2024-04-22 15:23:06 -0400

    added post fetching to homepage

--- app/(root)/[userId]/home/page.tsx
+++ app/(root)/[userId]/home/page.tsx
@@ -1,6 +1,11 @@
 import { Post } from "@/components/post";
+import prismadb from "@/lib/prismadb";
+export default async function Home() {
+  const posts = await prismadb.post.findMany({
+    orderBy: { createdAt: "desc" },
+    take: 10,
+  });
 
-export default function Home() {
   return (
     <main className="flex min-h-screen w-full max-w-2xl flex-col items-center">
       <Post />
