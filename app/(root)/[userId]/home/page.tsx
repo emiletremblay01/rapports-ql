71a7c35c1a8499ff6571d67819476a9b0d7e9e9b
Emile Tremblay <76021392+emiletremblay01@users.noreply.github.com>
2024-04-27 21:59:19 -0400

    changed overflow classname

--- app/(root)/[userId]/home/page.tsx
+++ app/(root)/[userId]/home/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   });
 
   return (
-    <main className="flex h-screen w-full max-w-2xl flex-col items-center overflow-scroll">
+    <main className="flex h-screen w-full max-w-2xl flex-col items-center overflow-y-auto">
       {posts.map((post) => (
         <Post key={post.id} post={post} />
       ))}
